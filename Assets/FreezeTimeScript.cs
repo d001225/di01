54672e88c5a2838ae5f0a851b447a6e39775cdc4
d001225 <d001225@ismai.pt>
2016-10-03 11:12:49 +0100

    teste 2

--- Assets/FreezeTimeScript.cs
+++ Assets/FreezeTimeScript.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 
 public class FreezeTimeScript : MonoBehaviour {
-    public void FreezeGame() { SetTimeScale(0); }
+    public void FreezeGame() {
+        SetTimeScale(0);
+    }
 
-    public void UnfreezeGame() { SetTimeScale(1); }
+    public void UnfreezeGame() {
+        SetTimeScale(1);
+    }
 
     private void SetTimeScale(float timeScale) { Time.timeScale = timeScale; }
 }
